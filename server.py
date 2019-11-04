97f8963cea9064dbfad6d6e1340d310d6930351f
Ruchit Desai <xitram@gmail.com>
2019-11-04 17:17:52 -0500

    change ip

--- server.py
+++ server.py
@@ -185,7 +185,7 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='35.196.136.212')
+  @click.argument('HOST', default='localhost')
   @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
