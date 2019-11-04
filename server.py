6d6df6a7f8431a0c7b34b0f01fe8ad82f0848a12
Ruchit Desai <xitram@gmail.com>
2019-11-04 17:16:01 -0500

    Updated IP

--- server.py
+++ server.py
@@ -14,8 +14,10 @@ import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
+import pdb; pdb.set_trace()
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+
 app = Flask(__name__, template_folder=tmpl_dir)
 
 
@@ -183,7 +185,7 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('HOST', default='35.196.136.212')
   @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
