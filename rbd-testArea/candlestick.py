2768068b335a60fe24568d741d11d59b0d67e5eb
Ryan Anderson <ryan.andersonk@gmail.com>
2019-11-15 16:42:57 -0500

    created routing function to index.html

--- rbd-testArea/candlestick.py
+++ rbd-testArea/candlestick.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime
 import sqlalchemy as db 
 import glob
+from flask import Flask, render_template
 
 #connect to database
 #DATABASEURI = "postgresql://rbd2127:group69db@35.243.220.243/proj1part2"
@@ -13,10 +14,20 @@ import glob
 #get dat file names
 df = pd.read_csv('https://github.com/ra2929/w4111-proj1/tree/master/csv_data_for_db/AAPL.csv')
 
-#create candlestick chart
+#implement function to iterate through CSV files and select the ticker to display graphically
+
+#create candlestick chart (Using AAPL as the first example)
 
 graph = go.Figure(data = [go.Candlestick(x=df['Date'], open = df['AAPL.Open'], high=df['AAPL.High'],
                         low = df['AAPL.Low'], close = df['AAPL.Close']
                         )])
 
 graph.show()
+
+#render webpage
+@app.route('/')
+def index():
+    return render_template("index.html", ticker = ticker, startDate = startDate, endDate = endDate, size = size)
+
+
+
